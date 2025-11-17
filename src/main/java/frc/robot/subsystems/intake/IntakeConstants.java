58716a115cc9b99a127857bad116422c7447c80b
Abigail-27 <168038538+Abigail-27@users.noreply.github.com>
2025-11-17 17:41:32 -0500

    woah (#2)

---
+++ src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
@@ -0,0 +1,10 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.intake;
+
+/** Add your docs here. */
+public class IntakeConstants {
+    public static int INTAKE_MOTOR_ID = 0-9;
+}
