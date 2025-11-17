68221eea052d141bebb5054975e1721a5fbb47e5
AndreaTheDemure <115323932+AndreaTheDemure@users.noreply.github.com>
2025-11-17 17:55:34 -0500

    Fixing number of motors (#4)
    * huh
    
    * :)
    
    * changed number of motors
    
    ---------
    
    Co-authored-by: aridavidson001 <aridavidson6@gmail.com>
    Co-authored-by: Abigail-27 <168038538+Abigail-27@users.noreply.github.com>

--- src/main/java/frc/robot/subsystems/drive/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/drive/DriveSubsystem.java
@@ -4,46 +4,31 @@
 
 package frc.robot.subsystems.drive;
 
-import com.fasterxml.jackson.annotation.JsonTypeInfo.Id;
 
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj.motorcontrol.PWMTalonFX;
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class DriveSubsystem extends SubsystemBase {
   /** Creates a new ExampleSubsystem. */
 
   public final DifferentialDrive tankDrive;
-  public MotorControllerGroup leftMotors;
-  public MotorControllerGroup rightMotors;
 
-   
-    public PWMTalonFX driveMotor1;
-    public PWMTalonFX driveMotor2;
-    public PWMTalonFX driveMotor3;
-    public PWMTalonFX driveMotor4;
-    public PWMTalonFX driveMotor5;
-    public PWMTalonFX driveMotor6;
-    @SuppressWarnings("removal")
-    public DriveSubsystem () {
-    
-      driveMotor1 = new PWMTalonFX(DriveConstants.DRIVE_MOTOR_1_ID);
-      driveMotor2 = new PWMTalonFX(DriveConstants.DRIVE_MOTOR_2_ID);
-      driveMotor3 = new PWMTalonFX(DriveConstants.DRIVE_MOTOR_3_ID);
-      driveMotor4 = new PWMTalonFX(DriveConstants.DRIVE_MOTOR_4_ID);
-      driveMotor5 = new PWMTalonFX(DriveConstants.DRIVE_MOTOR_5_ID);
-      driveMotor6 = new PWMTalonFX(DriveConstants.DRIVE_MOTOR_6_ID);
-      leftMotors = new MotorControllerGroup(driveMotor1, driveMotor2, driveMotor3);
-      rightMotors = new MotorControllerGroup(driveMotor4, driveMotor5, driveMotor6);
-      rightMotors.setInverted(true);
-      tankDrive = new DifferentialDrive(leftMotors, rightMotors);
-    }
   
-    public void driveCommand(double leftSpeed, double rightSpeed) {
-      tankDrive.arcadeDrive(leftSpeed, rightSpeed, true);
-    }
+  public PWMTalonFX driveMotor1;
+  public PWMTalonFX driveMotor2;
+  @SuppressWarnings("removal")
+  public DriveSubsystem () {
+  
+    driveMotor1 = new PWMTalonFX(DriveConstants.DRIVE_MOTOR_1_ID);
+    driveMotor2 = new PWMTalonFX(DriveConstants.DRIVE_MOTOR_2_ID);
+    driveMotor2.setInverted(true);
+    tankDrive = new DifferentialDrive(driveMotor1, driveMotor2);
+  }
+
+  public void driveCommand(double leftSpeed, double rightSpeed) {
+    tankDrive.arcadeDrive(leftSpeed, rightSpeed, true);
+  }
 
     /**
      * Example command factory method.
