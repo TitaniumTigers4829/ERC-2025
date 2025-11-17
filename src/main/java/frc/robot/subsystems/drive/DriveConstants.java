146ce14d3b198e3df99b81c659591cdae29232fd
aridavidson001 <aridavidson6@gmail.com>
2025-11-17 17:47:59 -0500

    changed number of motors

--- src/main/java/frc/robot/subsystems/drive/DriveConstants.java
+++ src/main/java/frc/robot/subsystems/drive/DriveConstants.java
@@ -8,9 +8,4 @@ package frc.robot.subsystems.drive;
 public class DriveConstants {
     public final static int DRIVE_MOTOR_1_ID = 0-9;
     public final static int DRIVE_MOTOR_2_ID = 0-9;
-    public final static int DRIVE_MOTOR_3_ID = 0-9;
-    public final static int DRIVE_MOTOR_4_ID = 0-9;
-    public final static int DRIVE_MOTOR_5_ID = 0-9;
-    public final static int DRIVE_MOTOR_6_ID = 0-9;
-
 }
