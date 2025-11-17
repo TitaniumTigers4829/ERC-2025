b146a7426e574dd182291dc2d865192d1daccf44
aridavidson001 <aridavidson6@gmail.com>
2025-11-17 17:08:06 -0500

    wtf

---
+++ src/main/java/frc/robot/subsystems/shooter/ShooterConstants.java
@@ -0,0 +1,6 @@
+package frc.robot.subsystems.shooter;
+
+public class ShooterConstants {
+    // the ID of the motor that shoots the thingy
+    public static final int MOTOR_ID = 0; // TODO: make this the actual ID
+}
--- src/main/java/frc/robot/subsystems/shooter/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterSubsystem.java
@@ -4,35 +4,17 @@
 
 package frc.robot.subsystems.shooter;
 
+
+import edu.wpi.first.wpilibj.motorcontrol.PWMTalonFX;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ShooterSubsystem extends SubsystemBase {
+  public PWMTalonFX shooterMotor; // the motor that spins the thingy to shoot the other thingy
+
   /** Creates a new ExampleSubsystem. */
-  public ShooterSubsystem() {}
-
-  /**
-   * Example command factory method.
-   *
-   * @return a command
-   */
-  public Command exampleMethodCommand() {
-    // Inline construction of command goes here.
-    // Subsystem::RunOnce implicitly requires `this` subsystem.
-    return runOnce(
-        () -> {
-          /* one-time action goes here */
-        });
-  }
-
-  /**
-   * An example method querying a boolean state of the subsystem (for example, a digital sensor).
-   *
-   * @return value of some boolean subsystem state, such as a digital sensor.
-   */
-  public boolean exampleCondition() {
-    // Query some boolean state, such as a digital sensor.
-    return false;
+  public ShooterSubsystem() {
+    shooterMotor = new PWMTalonFX(ShooterConstants.MOTOR_ID);
   }
 
   @Override
